b921c0cc221353024797f92bbaf53afdef4bc824
Aleena Tucker <thumbaleena@outlook.com>
2016-11-22 21:49:34 -0500

    added french-taunter script

---
+++ FirstASPapp/Scripts/french-taunter.js
@@ -0,0 +1,12 @@
+﻿var quotes = [];
+
+quotes.push("No, now go away or I shall taunt you a second time!");
+quotes.push("I don't want to talk to you no more, you empty-headed animal food trough wiper!");
+quotes.push("I fart in your general direction!");
+quotes.push("Go and boil your bottoms, you sons of a silly person.");
+quotes.push("I'm French. Why do you think I have this outrageous accent, you silly king?");
+
+function taunt() {
+    var quote = quotes[Math.floor(Math.random() * quotes.length)];
+    alert(quote);
+}
